1ab50fa02e218ef9f6e65b7cddad458c1e941318
sebastiancanonaco41 <96830910+sebastiancanonaco41@users.noreply.github.com>
2022-01-03 10:56:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
   res.send('Hello Worl!')
 })
 
+app.get('/today', (req, res) => {
+  res.send('yesterday')
+})
+
+app.get('/yesterday', (req, res) => {
+  res.send('yesterday')
+})
+
+app.get('/tomorrow', (req, res) => {
+  res.send('tomorrow')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
